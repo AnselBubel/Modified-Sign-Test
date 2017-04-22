8b951fa5d2039d508d042982cf7693340092b614
AnselBubel <ansel.bubel@gmail.com>
2017-04-22 10:25:53 -0400

    Create R file which contains the function

---
+++ ModSignTest.R
@@ -0,0 +1,9 @@
+###############################################          
+## Modified Sign Test                        ##       
+## Based on Cheng and Balakrishnan, 2004     ##
+## R implementation written by Ansel Bubel   ##
+###############################################
+
+ModSignTest <- function(){
+
+}
